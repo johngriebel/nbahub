540e4df109e1052430530418c7a0840bf23f082b
John Griebel <johnkgriebel@gmail.com>
2017-07-28 12:21:31 -0400

    Fix entry points.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     name="nbahub",
     packages=['nbahub', "nba_stats_api"],
     package_data={'nba_stats_api': ["data/bbref_id_map.json"]},
-    requires=["nba_py",
-              "requests==2.18.1",
-              "click==6.7",
-              "beautifulsoup4==4.6.0",
-              # Weird...
-              "html5lib==0.999999999",
-              "openpyxl==2.4.8"],
+    install_requires=["nba_py",
+                      "requests==2.18.1",
+                      "click==6.7",
+                      "beautifulsoup4==4.6.0",
+                      # Weird...
+                      "html5lib==0.999999999",
+                      "openpyxl==2.4.8"],
     version="0.1",
     license="MIT",
     description="A CLI application that gathers NBA stats from various sources and aggregates them.",
@@ -23,6 +23,6 @@ setup(
     classifiers=['Development Status :: 3 - Alpha',
                  'Programming Language :: Python :: 3.6'],
     entry_points={
-        'console_scripts': ["nbahub=nbahub_cli:main"]
+        'console_scripts': ["nbahub=nbahub.nbahub_cli:main"]
     }
 )
